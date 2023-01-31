66b0d6ad3aa822cb327a9d75386f5ad845198c0a
Tony Commendador <tony@laulimasystems.com>
2023-01-30 15:37:54 -1000

    Initial commit for select error when using interface popover

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,22 +2,55 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Select Error for Interface Popover</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+      <ion-select label="Select" v-model="selectedValueRef" interface="popover">
+        <ion-select-option
+          v-for="(value, index) in values"
+          :key="index"
+          :value="value"
+          >{{ value }}</ion-select-option
+        >
+      </ion-select>
+      <ion-input label="Selected value" v-model="selectedValueRef" readonly />
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonSelect,
+  IonSelectOption,
+  IonInput,
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "Tab1Page",
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonSelect,
+    IonSelectOption,
+    IonInput,
+  },
+  setup() {
+    const values = ["Foo", "Bar"];
+    const selectedValueRef = ref(values[0]);
+    return {
+      values,
+      selectedValueRef,
+    };
+  },
+});
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -7,16 +7,6 @@
           <ion-icon :icon="triangle" />
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
-
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
-        </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -24,5 +14,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { triangle } from 'ionicons/icons';
 </script>
